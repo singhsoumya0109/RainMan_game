2c6e8296b61d661ec74eee603da2f5a4aafde593
singhsoumya0109 <singhsoumya1103@gmail.com>
2024-11-02 21:45:05 +0530

    code updated

--- Umbrella.cpp
+++ Umbrella.cpp
@@ -1,44 +1,59 @@
+// #include "Umbrella.h"
+// #include<QBrush>
+// Umbrella::Umbrella(int x, int y, int width, int height, QObject *parent)
+//     : QObject(parent), QGraphicsRectItem(x, y, width, height) {
+//     setBrush(Qt::green); // Green umbrella
+//     // x_=x;
+//     // y_=y;
+//     this->width=width;
+//     this->height=height;
+//     location_right_now=x;
+// }
+
+// void Umbrella::moveLeft(int distance) {
+
+//     if(location_right_now-distance>=-width){
+//         setPos(x()-distance,y());
+//         location_right_now-=distance;
+//     }
+// }
+
+// void Umbrella::moveRight(int distance, int screenWidth) {
+
+//     if(location_right_now+distance<=screenWidth+width){
+//         setPos(x()+distance,y());
+//         location_right_now+=distance;
+//     }
+// }
+
+
+
+
 #include "Umbrella.h"
-#include<QBrush>
+#include <QBrush>
+
 Umbrella::Umbrella(int x, int y, int width, int height, QObject *parent)
     : QObject(parent), QGraphicsRectItem(x, y, width, height) {
     setBrush(Qt::green); // Green umbrella
-    // x_=x;
-    // y_=y;
-    this->width=width;
-    this->height=height;
-    location_right_now=x;
+    this->width = width;
+    this->height = height;
+    location_right_now = x;
 }
 
 void Umbrella::moveLeft(int distance) {
-
-    if(location_right_now-distance>=-width){
-        setPos(x()-distance,y());
-        location_right_now-=distance;
+    // Check if moving left will keep the umbrella within the left boundary (0)
+    if (location_right_now - distance >= 0) {
+        setPos(x() - distance, y());
+        location_right_now -= distance;
     }
 }
 
 void Umbrella::moveRight(int distance, int screenWidth) {
-
-    if(location_right_now+distance<=screenWidth+width){
-        setPos(x()+distance,y());
-        location_right_now+=distance;
+    // Check if moving right will keep the umbrella within the right boundary (screenWidth - umbrella width)
+    if (location_right_now + distance + width <= screenWidth) {
+        setPos(x() + distance, y());
+        location_right_now += distance;
     }
 }
 
 
-
-
-
-// qDebug() << x();
-// if (x_-distance>=-width) {
-//     setPos(x_ - distance, y_);
-//     qDebug() << "Moved left to:" << x_-distance;
-// }
-// if(x-distance>=-(x_)){
-//     setPos(x()-distance)
-// }
-// if (x_+ distance <= screenWidth+width) {
-//     setPos(x_ + distance, y_);
-//     qDebug() << "Moved right to:" << x_+distance;
-// }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QRandomGenerator>
 #include <QDebug>
 #include <QVBoxLayout>
+#include <QRandomGenerator>
+#include <cmath>
 
 // MainWindow::MainWindow(QWidget *parent)
 //     : QMainWindow(parent), ui(new Ui::MainWindow), score(0) {
@@ -59,8 +61,10 @@
 //     // gameTimer->start(50); // Update game state every 50 ms
 // }
 
+
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), score(0) {
+    : QMainWindow(parent), ui(new Ui::MainWindow), score(10) {
     ui->setupUi(this);
 
     // Create a new QWidget to act as the container for the layout
@@ -71,10 +75,10 @@ MainWindow::MainWindow(QWidget *parent)
 
     // Create the QGraphicsScene and view
     scene = new QGraphicsScene(this);
-    scene->setSceneRect(30, 30, 800, 600);
+    scene->setSceneRect(0, 0, 800, 600);  // Set scene to match view dimensions
 
     view = new QGraphicsView(scene, this);
-    view->setFixedSize(800, 600);
+    view->setFixedSize(800, 600);  // Ensure view is 800x600 as well
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setFocusPolicy(Qt::StrongFocus);
@@ -87,14 +91,14 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(container);
 
     // Create the umbrella
-    umbrella = new Umbrella(0, 0, 130, 20, this);
-    umbrella->setPos(270, 580); // Set the umbrella's initial position relative to the scene
+    umbrella = new Umbrella(350, 550, 100, 20, this);  // Set initial x, y, width, and height
+    umbrella->setPos(40, 30);  // Center umbrella within the bottom section of the scene
     scene->addItem(umbrella);
 
     // Set up the score label
     scoreLabel = new QLabel(view);
-    scoreLabel->setGeometry(650, 10, 150, 30); // Position at the top-right in view
-    scoreLabel->setText("Score: 0");
+    scoreLabel->setGeometry(650, 10, 150, 30);  // Position at the top-right in view
+    scoreLabel->setText("Score: 10");
     scoreLabel->setStyleSheet("QLabel { color : white; font-size: 18px; }");
 
     // Position the start button below the score label
@@ -105,12 +109,16 @@ MainWindow::MainWindow(QWidget *parent)
     // Initialize other components like timers if necessary
 }
 
+
 MainWindow::~MainWindow() {
     delete ui;
 }
 
+
+
 void MainWindow::spawnRaindrop() {
-    int x = QRandomGenerator::global()->bounded(0, 700); // Random x position for raindrop
+    int x = QRandomGenerator::global()->bounded(60, 700); // Random x position for raindrop
+    // int x =60;
     Raindrop *raindrop = new Raindrop(x, 0, 5, 20); // Create raindrop at random position
     raindrops.append(raindrop); // Add raindrop to list
     scene->addItem(raindrop); // Add raindrop to scene
@@ -120,7 +128,7 @@ void MainWindow::updateGame() {
     // Move raindrops down and check for collisions
     for (int i = 0; i < raindrops.size(); ++i) {
         Raindrop *raindrop = raindrops[i];
-        raindrop->moveDown(5); // Move the raindrop down by 5 units
+        raindrop->moveDown(15); // Move the raindrop down by 5 units
 
         // Check collision with umbrella
         if (raindrop->collidesWithItem(umbrella)) {
@@ -128,7 +136,7 @@ void MainWindow::updateGame() {
             raindrops.removeAt(i);
             delete raindrop;
             --i;
-            updateScore(1);
+            updateScore(-1);
             // Update score here if needed
 
         } else if (raindrop->isOutOfScreen(scene->height())) {
@@ -142,7 +150,7 @@ void MainWindow::updateGame() {
 
 void MainWindow::keyPressEvent(QKeyEvent *event) {
     qDebug() << "Key Pressed:" << event->key(); // Debug print to show the key code
-    int distance = 5; // Distance the umbrella moves
+    int distance = 15; // Distance the umbrella moves
 
     // Move the umbrella left or right using "A" and "D" keys
     if (event->key() == Qt::Key_A) { // A for left
@@ -154,183 +162,70 @@ void MainWindow::keyPressEvent(QKeyEvent *event) {
     }
 
 }
-void MainWindow::updateScore(int points) {
-    score += points;
-    scoreLabel->setText("Score: " + QString::number(score));
-}
-
-// Set up the scene
-// scene = new QGraphicsScene(this);
-// scene->setSceneRect(0, 0, 800, 600);
-
-// Set up the view
-
-// if (event->key() == Qt::Key_Left) {
-//     umbrella->moveLeft(distance);
-//     qDebug() << "Moved left";
-//     event->accept(); // Accept the event
-// } else if (event->key() == Qt::Key_Right) {
-//     umbrella->moveRight(distance, scene->width());
-//     qDebug() << "Moved right";
-//     event->accept(); // Accept the event
-// } else {
-//     event->ignore(); // Ignore other keys
-// }
-
-void MainWindow::on_pushButton_clicked()
-
-{
-    spawnTimer = new QTimer(this);
-    connect(spawnTimer, &QTimer::timeout, this, &MainWindow::spawnRaindrop);
-    spawnTimer->start(1000); // Spawn a raindrop every second
-
-    gameTimer = new QTimer(this);
-    connect(gameTimer, &QTimer::timeout, this, &MainWindow::updateGame);
-    gameTimer->start(50);
-
-}
-
-
-
-// #include "mainwindow.h"
-// #include "ui_mainwindow.h"
-// #include <QKeyEvent>
-// #include <QRandomGenerator>
-// #include <QDebug>
-// #include <QVBoxLayout>
-
-// MainWindow::MainWindow(QWidget *parent)
-//     : QMainWindow(parent), ui(new Ui::MainWindow), score(0) {
-//     ui->setupUi(this);
-
-//     ui->pushButton->show();
-//     ui->pushButton->setGeometry(0,0,10,10);
-//     scene = new QGraphicsScene(this);
-//     scene->setSceneRect(30, 30, 800, 600);
-//     QVBoxLayout *layout = new QVBoxLayout();
-//     layout->addWidget(view);           // Add the QGraphicsView to the layout
-//     layout->addWidget(ui->pushButton);
-//     view = new QGraphicsView(scene, this);
-//     view->setFixedSize(800, 600);
-//     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//     view->setFocusPolicy(Qt::StrongFocus);
-//     view->setScene(scene);
-//     setCentralWidget(view);
-//     view->setFocus(); // Set focus to the view
-
-//     // Create the umbrella
-//     umbrella = new Umbrella(0,0, 130, 20,this);
-//     // Create the umbrella
-//     // umbrella = new Umbrella(0, 0, 130, 20);
-//     umbrella->setPos(270,580); // Set the umbrella's initial position relative to the scene
-//     scene->addItem(umbrella);
-//     // scene->
-//     // scene->addItem(umbrella);
-//     // qDebug() << "Initial x position of umbrella:" << umbrella->x();
-//     // QPointF scenePos = umbrella->mapToScene(umbrella->rect().topLeft());
-//     // qDebug() << "Umbrella position in scene:" << scenePos;
-//     // qDebug() << "Umbrella's x position in scene:" << umbrella->x();
-//     // qDebug() << "Umbrella's y position in scene:" << umbrella->y();
-
-//     // QPointF localPos(umbrella->x(), umbrella->y());
-//     // QPointF scenePos = umbrella->mapToScene(localPos);
-//     // qDebug() << "Converted position in scene:" << scenePos;
-
-//     scoreLabel = new QLabel(this);
-//     scoreLabel->setGeometry(650, 10, 150, 30); // Position at the top-right
-//     scoreLabel->setText("Score: 0");
-//     scoreLabel->setStyleSheet("QLabel { color : black; font-size: 18px; }");
-
-
-//     // Timers for raindrop spawning and game updates
-//     spawnTimer = new QTimer(this);
-//     connect(spawnTimer, &QTimer::timeout, this, &MainWindow::spawnRaindrop);
-//     spawnTimer->start(1000); // Spawn a raindrop every second
-
-//     gameTimer = new QTimer(this);
-//     connect(gameTimer, &QTimer::timeout, this, &MainWindow::updateGame);
-//     gameTimer->start(50); // Update game state every 50 ms
-// }
-
-// MainWindow::~MainWindow() {
-//     delete ui;
-// }
-
-// void MainWindow::spawnRaindrop() {
-//     int x = QRandomGenerator::global()->bounded(0, 700); // Random x position for raindrop
-//     Raindrop *raindrop = new Raindrop(x, 0, 5, 20); // Create raindrop at random position
-//     raindrops.append(raindrop); // Add raindrop to list
-//     scene->addItem(raindrop); // Add raindrop to scene
-// }
-
-// void MainWindow::updateGame() {
-//     // Move raindrops down and check for collisions
-//     for (int i = 0; i < raindrops.size(); ++i) {
-//         Raindrop *raindrop = raindrops[i];
-//         raindrop->moveDown(5); // Move the raindrop down by 5 units
-
-//         // Check collision with umbrella
-//         if (raindrop->collidesWithItem(umbrella)) {
-//             scene->removeItem(raindrop);
-//             raindrops.removeAt(i);
-//             delete raindrop;
-//             --i;
-//             updateScore(1);
-//             // Update score here if needed
-
-//         } else if (raindrop->isOutOfScreen(scene->height())) {
-//             scene->removeItem(raindrop);
-//             raindrops.removeAt(i);
-//             delete raindrop;
-//             --i;
-//         }
-//     }
-// }
-
-// void MainWindow::keyPressEvent(QKeyEvent *event) {
-//     qDebug() << "Key Pressed:" << event->key(); // Debug print to show the key code
-//     int distance = 5; // Distance the umbrella moves
-
-//     // Move the umbrella left or right using "A" and "D" keys
-//     if (event->key() == Qt::Key_A) { // A for left
-//         umbrella->moveLeft(distance);
-//         qDebug() << "Moved left";
-//     } else if (event->key() == Qt::Key_D) { // D for right
-//         umbrella->moveRight(distance, scene->width());
-//         qDebug() << "Moved right";
-//     }
-
-// }
 // void MainWindow::updateScore(int points) {
 //     score += points;
 //     scoreLabel->setText("Score: " + QString::number(score));
 // }
 
-// // Set up the scene
-// // scene = new QGraphicsScene(this);
-// // scene->setSceneRect(0, 0, 800, 600);
 
-// // Set up the view
-
-// // if (event->key() == Qt::Key_Left) {
-// //     umbrella->moveLeft(distance);
-// //     qDebug() << "Moved left";
-// //     event->accept(); // Accept the event
-// // } else if (event->key() == Qt::Key_Right) {
-// //     umbrella->moveRight(distance, scene->width());
-// //     qDebug() << "Moved right";
-// //     event->accept(); // Accept the event
-// // } else {
-// //     event->ignore(); // Ignore other keys
-// // }
 
 // void MainWindow::on_pushButton_clicked()
 
 // {
+//     spawnTimer = new QTimer(this);
+//     connect(spawnTimer, &QTimer::timeout, this, &MainWindow::spawnRaindrop);
+//     spawnTimer->start(400); // Spawn a raindrop every second
 
-//     qDebug()<<"hi";
+//     gameTimer = new QTimer(this);
+//     connect(gameTimer, &QTimer::timeout, this, &MainWindow::updateGame);
+//     gameTimer->start(100);
+
 // }
 
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
 
+void MainWindow::updateScore(int points) {
+    score += points;
+    scoreLabel->setText("Score: " + QString::number(score));
+
+    //Check if score has reached zero
+    if (score <= 0) {
+        endGame();
+    }
+}
+
+void MainWindow::endGame() {
+    // Stop the timers to end the game
+    if (spawnTimer) {
+        spawnTimer->stop();
+    }
+    if (gameTimer) {
+        gameTimer->stop();
+    }
+
+    // Show a message box to indicate the game is over
+    QMessageBox::information(this, "Game Over", "You have lost. Score reached zero.");
+
+    // Optionally, reset the game or disable further actions
+    // score = 10; // Reset score or perform any other reset actions if desired
+    // scoreLabel->setText("Score: " + QString::number(score));
+    // raindrops.clear();
+}
+
+void MainWindow::on_pushButton_clicked() {
+    // Reset the score at the start of the game
+    score = 10;
+    scoreLabel->setText("Score: " + QString::number(score));
+
+    // Initialize and start timers
+    spawnTimer = new QTimer(this);
+    connect(spawnTimer, &QTimer::timeout, this, &MainWindow::spawnRaindrop);
+    spawnTimer->start(400); // Spawn a raindrop every 400 ms
+
+    gameTimer = new QTimer(this);
+    connect(gameTimer, &QTimer::timeout, this, &MainWindow::updateGame);
+    gameTimer->start(100); // Update game state every 100 ms
+}
 
