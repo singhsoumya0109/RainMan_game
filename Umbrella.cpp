e10c3b3f9adaf9b980aa4b791eb97fb2584bdf78
singhsoumya0109 <singhsoumya1103@gmail.com>
2024-11-18 10:01:58 +0530

    Update Umbrella.cpp

--- Umbrella.cpp
+++ Umbrella.cpp
@@ -1,34 +1,3 @@
-// #include "Umbrella.h"
-// #include<QBrush>
-// Umbrella::Umbrella(int x, int y, int width, int height, QObject *parent)
-//     : QObject(parent), QGraphicsRectItem(x, y, width, height) {
-//     setBrush(Qt::green); // Green umbrella
-//     // x_=x;
-//     // y_=y;
-//     this->width=width;
-//     this->height=height;
-//     location_right_now=x;
-// }
-
-// void Umbrella::moveLeft(int distance) {
-
-//     if(location_right_now-distance>=-width){
-//         setPos(x()-distance,y());
-//         location_right_now-=distance;
-//     }
-// }
-
-// void Umbrella::moveRight(int distance, int screenWidth) {
-
-//     if(location_right_now+distance<=screenWidth+width){
-//         setPos(x()+distance,y());
-//         location_right_now+=distance;
-//     }
-// }
-
-
-
-
 #include "Umbrella.h"
 #include <QBrush>
 
